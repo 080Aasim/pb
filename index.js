9a68cd8bce9cfa9facf6eb560ef8c6cb0611193d
Aasim <aasim9155@gmail.com>
2026-03-05 02:22:32 +0530

    1st Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors"
+import userRouter from "./routes/userRouter.js";
+import connectDb from "./config/mongodb.js";
+
+const app = express();
+const port = process.env.PORT || 4000;
+connectDb();
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/api/user",userRouter)
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+// export default app;
+app.listen(port, () => console.log("Server run at" + port))
